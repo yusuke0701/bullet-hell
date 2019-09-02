9fadd07f5861821db6db8c02ca807422a8f2a3bc
中島悠輔 <yusuke.nakashima@topgate.co.jp>
2019-09-02 12:09:16 +0900

    アセットローダークラスを追加

--- engine.js
+++ engine.js
@@ -25,3 +25,34 @@ class Sprite {
         this.rectangle = rectangle;
     }
 }
+
+// アセットローダークラスは、アセットを読み込む役割をもつ
+class AssetLoader {
+    constructor() {
+        this._promises = [];
+        this._assets = new Map();
+    }
+
+    addImage(name, url) {
+        const img = new Image();
+        img.src = url;
+
+        const promise = new Promise((resolve, reject) =>
+            img.addEventListener('load', (e) => {
+                this._assets.set(name, img);
+                resolve(img);
+            }));
+
+        this._promises.push(promise);
+    }
+
+    loadAll() {
+        return Promise.all(this._promises).then((p) => this._assets);
+    }
+
+    get(name) {
+        return this._assets.get(name);
+    }
+}
+
+const assets = new AssetLoader();
