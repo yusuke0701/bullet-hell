7fb2c72a0447c570508b5a7e8160ccf51b2f8097
中島悠輔 <yusuke.nakashima@topgate.co.jp>
2019-09-06 18:58:34 +0900

    自機を用意

---
+++ src/app/bullet-hell/fighter.js
@@ -0,0 +1,27 @@
+import { Sprite, SpriteActor, Rectangle } from '../game-engine';
+
+// ファイターは自機を表す
+export class Fighter extends SpriteActor {
+    constructor(x, y) {
+        const sprite = new Sprite(assets.get('sprite'), new Rectangle(0, 0, 16, 16));
+        const hitArea = new Rectangle(8, 8, 2, 2);
+        super(x, y, sprite, hitArea);
+
+        this.speed = 2;
+    }
+
+    update(gameInfo, input) {
+        if (input.getKey('ArrowUp')) {
+            this.y -= this.speed;
+        }
+        if (input.getKey('ArrowDown')) {
+            this.y += this.speed;
+        }
+        if (input.getKey('ArrowRight')) {
+            this.x += this.speed;
+        }
+        if (input.getKey('ArrowLeft')) {
+            this.x -= this.speed;
+        }
+    }
+}
