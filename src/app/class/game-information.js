2ab05aa8cf3bf0e8db5c6773fda1aecca8764e72
中島悠輔 <yusuke.nakashima@topgate.co.jp>
2019-09-06 11:20:33 +0900

    ゲームインフォメーションクラスを追加

---
+++ src/app/class/game-information.js
@@ -0,0 +1,8 @@
+export class GameInformation {
+    constructor(title, screenRectangle, maxFps, currentFps) {
+        this.title = title;
+        this.screenRectangle = screenRectangle;
+        this.maxFps = maxFps;
+        this.currentFps = currentFps;
+    }
+}
