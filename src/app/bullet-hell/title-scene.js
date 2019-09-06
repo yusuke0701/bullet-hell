676acea8fde23f2da7c66414ff3296987fa65f32
中島悠輔 <yusuke.nakashima@topgate.co.jp>
2019-09-06 19:05:11 +0900

    タイトルシーンを用意

---
+++ src/app/bullet-hell/title-scene.js
@@ -0,0 +1,18 @@
+import { Scene } from '../game-engine';
+import { DanmakuStgMainScene } from './main-scene';
+
+export class DanmakuStgTitleScene extends Scene {
+    constructor(renderingTarget) {
+        super('タイトル', 'black', renderingTarget);
+        const title = new Title(100, 200);
+        this.add(title);
+    }
+
+    update(gameInfo, input) {
+        super.update(gameInfo, input);
+        if (input.getKeyDown(' ')) {
+            const mainScene = new DanmakuStgMainScene(this.renderingTarget);
+            this.changeScene(mainScene);
+        }
+    }
+}
