3bd79f8f54e27fc8c98783ba2ef7862609015dd7
yusuke-home <yuusukerego@yahoo.co.jp>
2019-09-14 22:29:15 +0900

    ファイタークラスを修正して弾を撃てるようにした

--- src/app/bullet-hell/fighter.js
+++ src/app/bullet-hell/fighter.js
@@ -1,4 +1,5 @@
 import { Sprite, SpriteActor, Rectangle, assets } from '../game-engine';
+import { Bullet } from './bullet';
 
 // ファイターは自機を表す
 export class Fighter extends SpriteActor {
@@ -10,21 +11,51 @@ export class Fighter extends SpriteActor {
     const hitArea = new Rectangle(8, 8, 2, 2);
     super(x, y, sprite, hitArea);
 
-    this.speed = 2;
+    this._interval = 5;
+    this._timeCount = 0;
+    this._speed = 3;
+    this._velocityX = 0;
+    this._velocityY = 0;
   }
 
   update(gameInfo, input) {
+    // キーを押されたら移動する
+    this._velocityX = 0;
+    this._velocityY = 0;
+
     if (input.getKey('ArrowUp')) {
-      this.y -= this.speed;
+      this._velocityY = -this._speed;
     }
     if (input.getKey('ArrowDown')) {
-      this.y += this.speed;
+      this._velocityY = this._speed;
     }
     if (input.getKey('ArrowRight')) {
-      this.x += this.speed;
+      this._velocityX = this._speed;
     }
     if (input.getKey('ArrowLeft')) {
-      this.x -= this.speed;
+      this._velocityX = -this._speed;
+    }
+
+    this.x += this._velocityX;
+    this.y += this._velocityY;
+
+    // 画面外に行ってしまったら押し戻す
+    const boundWidth = gameInfo.screenRectangle.width - this.width;
+    const boundHeight = gameInfo.screenRectangle.height - this.height;
+    const bound = new Rectangle(this.width, this.height, boundWidth, boundHeight);
+
+    if (this.isOutOfBounds(bound)) {
+      this.x -= this._velocityX;
+      this.y -= this._velocityY;
+    }
+
+    // スペースキーで弾を打つ
+    this._timeCount++;
+    const isFireReady = this._timeCount > this._interval;
+    if (isFireReady && input.getKey(' ')) {
+      const bullet = new Bullet(this.x, this.y);
+      this.spawnActor(bullet);
+      this._timeCount = 0;
     }
   }
 }
