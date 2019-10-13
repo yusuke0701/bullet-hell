0130a7c07ccab38b4227c0121f6658a067f0b32a
yusuke-home <yuusukerego@yahoo.co.jp>
2019-10-13 22:39:34 +0900

    Enemyが動いて弾を撃てるようにした

--- src/app/bullet-hell/enemy.js
+++ src/app/bullet-hell/enemy.js
@@ -1,4 +1,5 @@
 import { SpriteActor, Sprite, assets, Rectangle, GameEvent } from '../game-engine';
+import { EnemyBullet } from './enemy-bullet';
 
 export class Enemy extends SpriteActor {
     constructor(x, y) {
@@ -12,6 +13,10 @@ export class Enemy extends SpriteActor {
         this.maxHp = 50;
         this.currentHp = this.maxHp;
 
+        this._interval = 120;
+        this._timeCount = 0;
+        this._velocityX = 0.3;
+
         // プレイヤーの弾に当たったらHPを減らす
         this.addEventListener('hit', e => {
             if (e.target.hasTag('playerBullet')) {
@@ -20,8 +25,38 @@ export class Enemy extends SpriteActor {
             }
         });
     }
+    // degree度の方向にspeedの速さで弾を発射する
+    shootBullet(degree, speed) {
+        const rad = degree / 180 * Math.PI;
+        const velocityX = Math.cos(rad) * speed;
+        const velocityY = Math.sin(rad) * speed;
+
+        const bullet = new EnemyBullet(this.x, this.y, velocityX, velocityY);
+        this.spawnActor(bullet);
+    }
+
+    // num個の弾を円形に発射する
+    shootCircularBullets(num, speed) {
+        const degree = 360 / num;
+        for (let i = 0; i < num; i++) {
+            this.shootBullet(degree * i, speed);
+        }
+    }
 
     update(gameInfo, input) {
+        // 左右に移動する
+        this.x += this._velocityX;
+        if (this.x <= 100 || this.x >= 200) {
+            this._velocityX *= -1;
+        }
+
+        // インターバルを経過していたら弾を撃つ
+        this._timeCount++;
+        if (this._timeCount > this._interval) {
+            this.shootCircularBullets(15, 1);
+            this._timeCount = 0;
+        }
+
         // HPがゼロになったらdestroyする
         if (this.currentHp <= 0) {
             this.destroy();
