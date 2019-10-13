346cb29ee2b1977e7ece945fdce82e605d1eaa19
yusuke-home <yuusukerego@yahoo.co.jp>
2019-10-13 22:10:47 +0900

    メインシーンに敵とHPバーを追加

--- src/app/bullet-hell/main-scene.js
+++ src/app/bullet-hell/main-scene.js
@@ -1,10 +1,16 @@
 import { Scene } from '../game-engine';
 import { Fighter } from './fighter';
+import { Enemy } from './enemy';
+import { EnemyHpBar } from './enemy-hp-bar';
 
 export class DanmakuStgMainScene extends Scene {
   constructor(renderingTarget) {
     super('メイン', 'black', renderingTarget);
     const fighter = new Fighter(150, 300);
+    const enemy = new Enemy(150, 100);
+    const hpBar = new EnemyHpBar(50, 20, enemy);
     this.add(fighter);
+    this.add(enemy);
+    this.add(hpBar);
   }
 }
