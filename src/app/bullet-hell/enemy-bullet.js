97d03d2f839a31e76541eeec1d1b18e9cca6d80b
yusuke-home <yuusukerego@yahoo.co.jp>
2019-10-13 22:26:53 +0900

    EnemyBulletクラスの追加

---
+++ src/app/bullet-hell/enemy-bullet.js
@@ -0,0 +1,24 @@
+import { SpriteActor, assets, Rectangle } from "../game-engine";
+
+export class EnemyBullet extends SpriteActor {
+    constructor(x, y, velocityX, velocityY) {
+        const sprite = new Sprite(
+            assets.get('sprite'),
+            new Rectangle(16, 16, 16, 16)
+        );
+        const hitArea = new Rectangle(4, 4, 8, 8);
+        super(x, y, sprite, hitArea, ['enemyBullet']);
+
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+    }
+
+    update(gameInfo, input) {
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+
+        if (this.isOutOfBounds(gameInfo.screenRectangle)) {
+            this.destroy();
+        }
+    }
+}
